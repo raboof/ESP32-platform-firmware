111f866c810d83b47fa5445377795ffc9ac818f0
Arnout Engelen <arnout@bzzt.net>
2019-10-09 20:49:14 +0200

    patchelf

--- badgeteam.firmware.nix
+++ badgeteam.firmware.nix
@@ -1,15 +1,25 @@
 with (import <nixpkgs> {});
-pkgs.mkShell {
+mkShell {
   name = "badgeteam.firmware";
-  builder = "${bash}/bin/bash";
-  args = [ ./builder.sh ];
-  setup = ./setup.sh;
-  system = builtins.currentSystem;
 
   buildInputs = [
     unzip gnutar
     coreutils bash gnumake git python gcc gnused
     binutils-unwrapped ncurses.dev which bison flex
-    gperf pkg-config gcc-unwrapped.lib
+    gperf pkg-config stdenv.cc.cc.lib
   ];
+
+  shellHook = let
+    libPath = lib.makeLibraryPath [
+      stdenv.cc.cc.lib zlib
+    ];
+  in ''
+    # yolo
+    for i in ./xtensa-esp32-elf/bin/* ./xtensa-esp32-elf/libexec/gcc/xtensa-esp32-elf/5.2.0/*; do \
+    patchelf \
+      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+      --set-rpath "${libPath}" \
+      $i; \
+    done
+    '';
 }
